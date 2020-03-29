64f89e2470b92d76025fc9f547c81d615316a80e
Dustin Kline <klinetek@gmail.com>
2020-03-29 15:05:40 -0400

    made a while loop and a number of tries!  this is cool

--- guessing_game.py
+++ guessing_game.py
@@ -1,20 +1,28 @@
 import random
 
-highest = 10
+highest = 30
 answer = random.randint(1,highest)
+number_of_geusses = 5
 
-print("Please guess a number between 1 and 10: ")
+print("Please guess a number between 1 and {}:".format(highest))
 guess = int(input())
-
 if guess == answer:
     print("wow you got it first try! look at you...")
 else:
     if guess < answer:
-        print("guess a little higher thist time")
+        print("No, Higher this time...")
     else:
-        print("guess just a bit lower")
-    guess = int(input())
-    if guess == answer:
-        print("Now you got it!  Nice.")
-    else:
-        print("Sorry, you're all out of tries bud.  Time to die. ")
+        print("No, Lower this time...")
+
+    while number_of_geusses != 0:
+        guess = int(input())
+        if guess == answer:
+            print("Now you got it!  Nice.")
+            break
+        else:
+            number_of_geusses -= 1
+            print("Nope, you have {} tries remaining".format(number_of_geusses))
+            if guess < answer:
+                print("No, Higher this time...")
+            else:
+                print("No, Lower this time...")
