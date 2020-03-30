8fa6fc439172e95192a6ef5286a86c65919853bb
Dustin Kline <klinetek@gmail.com>
2020-03-29 21:25:00 -0400

    uptodate

---
+++ Binary-Search.py
@@ -0,0 +1,22 @@
+low = 1
+high = 1000000
+guess = 1
+actual_guesses = 1
+print("I am going to guess your number."
+     " Think of a number between {} and {}".format(low, high))
+input("When you're ready, press the ENTER Key and I'll start.")
+
+while True:
+    guess = low + (high - low) // 2
+    high_low = input("my guess is {}. Should i guess higher or lower?"
+                     " Enter an H, L, or C if my guess was correct ". format(guess)).casefold()
+    if high_low == "h":
+        low = guess + 1
+    elif high_low == "l":
+        high = guess - 1
+    elif high_low == "c":
+        print("I got it in {} guesses!".format(actual_guesses))
+        break
+    else:
+        print("Please enter an H, L, or C ")
+    actual_guesses += 1
--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,8 @@
 import random
 
 highest = 30
-answer = random.randint(1,highest)
-number_of_geusses = 5
+answer = random.randint(1, highest)
+number_of_guesses = 5
 
 print("Please guess a number between 1 and {}:".format(highest))
 guess = int(input())
@@ -14,14 +14,14 @@ else:
     else:
         print("No, Lower this time...")
 
-    while number_of_geusses != 0:
+    while number_of_guesses != 0:
         guess = int(input())
         if guess == answer:
             print("Now you got it!  Nice.")
             break
         else:
-            number_of_geusses -= 1
-            print("Nope, you have {} tries remaining".format(number_of_geusses))
+            number_of_guesses -= 1
+            print("Nope, you have {} tries remaining".format(number_of_guesses))
             if guess < answer:
                 print("No, Higher this time...")
             else:
