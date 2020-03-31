16dce3ea9a70092963db1519b0ed914de002fb66
kline <klinetek@gmail.com>
2020-03-30 20:11:55 -0400

    tuples complete

--- Section-5-lists-ranges-tuples/tuples.py
+++ Section-5-lists-ranges-tuples/tuples.py
@@ -1,5 +1,21 @@
-t = "a", "b", "c"
-print(t)
+#t = "a", "b", "c"
+#print(t)
 
-print("a", "b", "c")
-print(("a", "b", "c"))
+#print("a", "b", "c")
+#print(("a", "b", "c"))
+
+welcom = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company" "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Imelda May", 2011, ((1, "Pulling the Rug"), (2, "Psycho")
+    , (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+print(imelda)
+
+title, artist, year, tracks = imelda
+print(title)
+print(artist)
+print(year)
+print(tracks)
+"""items in a tuple cannot change *however* if a mutable container is in your tuple
+then you can change the mutable item inside the tuple"""
