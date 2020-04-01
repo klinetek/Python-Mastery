7fe1c9af2f1ef5ba87047c6c3f31462a10d3d432
kline <klinetek@gmail.com>
2020-04-01 19:10:47 -0400

    challenge for io

--- I-O/iochallenge.py
+++ I-O/iochallenge.py
@@ -1,5 +1,6 @@
 """make a times table for the number 2 featuring 1*2-12*2.  then, append said
 table to the end of the jaborwalky text in sample.txt without breaking it."""
-
-for i in range(0, 13):
-    print("{:>} times 2 is {}".format(i, i*2))
+times_tables = []
+with open("sample.txt", 'a')as ifile:
+    for i in range(0, 13):
+        times_tables.append("{} times 2 is {}".format(i, i*2))
