3fcde7177bdb7b9e36d327b2f1b7f943b6959e9c
kline <klinetek@gmail.com>
2020-04-02 21:47:23 -0400

    really cool stuff!!  modules

---
+++ Modules/browser.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+webbrowser.open("https://www.python.org/") #this opens that webpage with default browser.
+
+help(webbrowser)
--- Modules/module_import.py
+++ Modules/module_import.py
@@ -1,10 +1,13 @@
 import turtle
-import tkinter as TK
-
+import time
+"""realizing i could actually make a game out of this or a screensaver it would
+neat to attatch the move commands to key input!  Just a thought. :) OH, i could
+also do the pipes screensaver from xp; that would be really cool to do if i was
+so inclined"""
 
 turtle.forward(150)
 turtle.right(250)
 turtle.circle(75)
 turtle.forward(150)
 
-turtle.done()
+time.sleep(4)
